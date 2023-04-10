5313d6c63986b4414df4acf0b910fd7ef1db02cc
Vincent Haron C. Mamutuk <vhcblade@gmail.com>
2023-04-10 21:39:08 +0800

    Added Move Up and Down Reorderable Test

--- test/reorderable_test.dart
+++ test/reorderable_test.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:event_bloc_tester/event_bloc_tester.dart';
 import 'package:event_db/event_db.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -7,6 +9,8 @@ import 'models.dart';
 void main() {
   group("Reorderable", () {
     group("Insert", insertTest);
+    group("Move Down", moveDownTest);
+    group("Move Up", moveUpTest);
   });
 }
 
@@ -28,12 +32,76 @@ Future<GenericModelMap<ExampleReorderableModel>> setupMap(
   return map;
 }
 
+void moveUpTest() {
+  final tester = SerializableListTester<List<ExampleReorderableModel>>(
+    testGroupName: "Reorderable",
+    mainTestName: "Move Up",
+    // mode: ListTesterMode.generateOutput,
+    mode: ListTesterMode.testOutput,
+    testFunction: (value, tester) async {
+      final map = await setupMap(value);
+      await map.addModel(map.setOrdinalOfNewEntry(
+          ExampleReorderableModel()..name = "Inserted One"));
+
+      map.reorder(map.defaultOrderedList[min(100, map.map.length - 1)], 0);
+      tester.addTestValue(map.defaultOrderedList.map((e) => e.name).toList());
+
+      map.reorder(map.defaultOrderedList[min(100, map.map.length - 1)], 3);
+      tester.addTestValue(map.defaultOrderedList.map((e) => e.name).toList());
+
+      map.reorder(map.defaultOrderedList[min(5, map.map.length - 1)], 0);
+      tester.addTestValue(map.defaultOrderedList.map((e) => e.name).toList());
+
+      map.reorder(map.defaultOrderedList[min(8, map.map.length - 1)], 4);
+      tester.addTestValue(map.defaultOrderedList.map((e) => e.name).toList());
+
+      map.reorder(map.defaultOrderedList[min(9, map.map.length - 1)], 7);
+      tester.addTestValue(map.defaultOrderedList.map((e) => e.name).toList());
+    },
+    testMap: commonTestCases,
+  );
+
+  tester.runTests();
+}
+
+void moveDownTest() {
+  final tester = SerializableListTester<List<ExampleReorderableModel>>(
+    testGroupName: "Reorderable",
+    mainTestName: "Move Down",
+    // mode: ListTesterMode.generateOutput,
+    mode: ListTesterMode.testOutput,
+    testFunction: (value, tester) async {
+      final map = await setupMap(value);
+      await map.addModel(map.setOrdinalOfNewEntry(
+          ExampleReorderableModel()..name = "Inserted One"));
+
+      map.reorder(map.defaultOrderedList[min(0, map.map.length - 1)], 100);
+      tester.addTestValue(map.defaultOrderedList.map((e) => e.name).toList());
+
+      map.reorder(map.defaultOrderedList[min(0, map.map.length - 1)], 3);
+      tester.addTestValue(map.defaultOrderedList.map((e) => e.name).toList());
+
+      map.reorder(map.defaultOrderedList[min(5, map.map.length - 1)], 8);
+      tester.addTestValue(map.defaultOrderedList.map((e) => e.name).toList());
+
+      map.reorder(map.defaultOrderedList[min(2, map.map.length - 1)], 4);
+      tester.addTestValue(map.defaultOrderedList.map((e) => e.name).toList());
+
+      map.reorder(map.defaultOrderedList[min(1, map.map.length - 1)], 1000);
+      tester.addTestValue(map.defaultOrderedList.map((e) => e.name).toList());
+    },
+    testMap: commonTestCases,
+  );
+
+  tester.runTests();
+}
+
 void insertTest() {
   final tester = SerializableListTester<List<ExampleReorderableModel>>(
-    testGroupName: "File Commands",
-    mainTestName: "createFileObject",
-    mode: ListTesterMode.generateOutput,
-    // mode: ListTesterMode.testOutput,
+    testGroupName: "Reorderable",
+    mainTestName: "Insert",
+    // mode: ListTesterMode.generateOutput,
+    mode: ListTesterMode.testOutput,
     testFunction: (value, tester) async {
       final map = await setupMap(value);
       await map.addModel(map.setOrdinalOfNewEntry(
